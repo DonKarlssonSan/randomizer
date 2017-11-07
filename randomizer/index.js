94d43699790946859276a153288d33b113736343
Johan Karlsson <DonKarlssonSan@gmail.com>
2017-11-07 22:22:33 +0100

    Initial commit

---
+++ randomizer/index.js
@@ -0,0 +1,22 @@
+const crypto = require("crypto");
+
+module.exports = function (context, req) {
+    const buf = crypto.randomBytes(16);
+    context.log(`${buf.length} bytes of random data: ${buf.toString("hex")}`);
+    let acceptLanguage = req.headers["accept-language"];
+    let locale = "sv-SE";
+    if(acceptLanguage) {
+        let langs = acceptLanguage.split(",");
+        if(langs[0]) {
+            locale = langs[0];
+        }
+    }
+    context.log(`Locale: ${locale}`);
+    let number = buf.readUInt16BE().toLocaleString(locale);
+    context.log(`Number: ${number}`);
+
+    context.res = {
+        body: number
+    };
+    context.done();
+};
